080ed0b0c8f29d2e1aa3295af0a1251ec8e773a9
Yiran Zhou <yiran@smartx.com>
2026-02-27 17:47:20 +0800

    ui: remove Reply Times legend from tree visualization
    - Remove legend HTML from page.ts
    - Remove .legend and .legend-item CSS styles

--- src/viewer/page.ts
+++ src/viewer/page.ts
@@ -2,13 +2,6 @@ export function createPage(container: HTMLElement) {
     let pageHTML = `
         <div id="treeContainer">
             <svg id="tree"></svg>
-            <div class="legend">
-                <span class="text-xs font-medium mb-1 block">Reply Times</span>
-                <span class="legend-item"><span class="w-2 h-2 rounded-full bg-rose-500"></span>5m</span>
-                <span class="legend-item"><span class="w-2 h-2 rounded-full bg-yellow-400"></span>10m</span>
-                <span class="legend-item"><span class="w-2 h-2 rounded-full bg-amber-100"></span>1h</span>
-                <span class="legend-item"><span class="w-2 h-2 rounded-full bg-cyan-400"></span>3h+</span>
-            </div>
         </div>
         <div id="sidebar" class="bg-base-200">
             <div id="infoBox" class="hidden">
